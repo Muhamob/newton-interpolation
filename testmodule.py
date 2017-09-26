0f60535698ae4fd0c12388e027a5e9ea290fb85c
Muhamob <muhamob@gmail.com>
2017-09-26 22:10:25 +0300

    Update testmodule.py

--- testmodule.py
+++ testmodule.py
@@ -9,7 +9,6 @@ def test_for_class_impl(start=0, stop=5, h=.13):
     f_x = lambda x: x*np.sin(x**2)
     f = f_x(x)
     #coefs = newton.div_diff_coefs(x, f)
-    #print(coefs)
     x_inter = np.arange(start+.1, stop-.01, h)
     f_interpol = newton.newton_poly(x_inter, x0 = x, f0 = f)
     plt.plot(x, f, label='true')
